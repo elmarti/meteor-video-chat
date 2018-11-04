af150ba9e2826e4c05dac88243971ff98dc7bee4
Jack <info@elmarti.tech>
2018-11-04 10:33:43 +0000

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 //jshint esversion: 6
 Package.describe({
     name: 'elmarti:video-chat',
-    version: '2.3.1',
+    version: '2.3.2',
     summary: 'Simple WebRTC Video Chat for your app.',
     git: 'https://github.com/elmarti/meteor-video-chat',
     documentation: 'README.md'
